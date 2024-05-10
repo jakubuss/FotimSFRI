ec878bf5607d5aa049bdfe51ee031ca5f585c1e5
Jakub <nedza.80@gmail.com>
2024-05-10 11:37:27 +0200

    Initial Commit

---
+++ FotimSFRIv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FotimSFRIv2
+//
+//  Created by Jakub on 10/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FotimSFRIv2/FotimSFRIv2App.swift
@@ -0,0 +1,17 @@
+//
+//  FotimSFRIv2App.swift
+//  FotimSFRIv2
+//
+//  Created by Jakub on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FotimSFRIv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
