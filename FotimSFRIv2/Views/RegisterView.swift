4b9dd69b4c72c535a0a832eeaef48b023edc7f4a
Jakub <nedza.80@gmail.com>
2024-05-11 18:03:22 +0200

    buttny na register
    buttony na register a idem behat

--- FotimSFRIv2/Views/RegisterView.swift
+++ FotimSFRIv2/Views/RegisterView.swift
@@ -7,7 +7,12 @@
 
 import SwiftUI
 
-struct RegisterView: View {
+struct RegisterView: View { // meno(dlhe tak prezyvka) - mail - heslo - accept
+    @State var prezyvka = ""
+    @State var email = ""
+    @State var heslo = ""
+    
+    
     var body: some View {
         VStack{
             HlavickaView(nadpis: "Registrácia",
@@ -18,6 +23,38 @@ struct RegisterView: View {
             Spacer()
             
         }
+        Form{
+            TextField("Zadaj Prezývku", text: $prezyvka)
+                .textFieldStyle(DefaultTextFieldStyle())
+            TextField("Zadaj Email", text: $email)
+                .textFieldStyle(DefaultTextFieldStyle())
+            SecureField("Zadaj Heslo", text: $heslo)
+                .textFieldStyle(DefaultTextFieldStyle())
+            
+            Button{
+                
+            } label:{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .foregroundColor(Color.yellow)
+                    Text("Registruj")
+                        .foregroundColor(Color.blue)
+                        .bold()
+                }
+            }
+        }
+        VStack{
+            Text("Máš už účet?")
+            //Button("Vytvor účet") { //prechod do registracie
+            NavigationLink("Vráť ma späť!", destination: LoginView())
+            //self.presentationMode.wrappedValue.dismiss()
+                .bold()
+                
+            
+        }
+        .padding(.bottom, 20)
+        //Guest prihlasenie (eh asi nie, skor prihlasit a base prava) / registracia
+        Spacer()
         
     }
 }
